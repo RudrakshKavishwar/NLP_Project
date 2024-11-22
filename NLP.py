12eb4b5069735bd9de3fc151ac224a42884e7b09
RudrakshKavishwar <160317602+RudrakshKavishwar@users.noreply.github.com>
2024-11-22 10:32:04 +0530

    Update NLP.py

--- NLP.py
+++ NLP.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import PyPDF2
 from pyresparser import ResumeParser
 from sklearn.neighbors import NearestNeighbors
 from src.components.job_recommender import ngrams,getNearestN,jd_df
